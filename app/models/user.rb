238f48640f88b9a19e0e4ec15f5f51a385fb0c8a
Charlie Whiteside <charlie.whiteside@rocksteadymusicschool.com>
2023-05-31 11:02:23 +0100

    Add role to user model & create role checking methods on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 class User < ApplicationRecord
+  ROLES = %w{ client admin }
+  
   has_secure_password
   validates :username, uniqueness: true
+  validates :role, inclusion: { in: ROLES  }
   has_many :pads
   has_many :frames
+
 
   after_create do
     self.pads.create({
       user_id: self.id,
       name: 'My Canvasses'
     })
+  end
+
+  ROLES.each do |role_name|
+    define_method "#{role_name}?" do
+     self.role == role_name 
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_103209) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_080935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_103209) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "client", null: false
   end
 
   add_foreign_key "frames", "pads"
